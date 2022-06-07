c5b1ffccdd3a7685bae10270a88c3a517be258ff
nk400 <navkr007@gmail.com>
2022-06-07 07:55:24 +0530

    ADDED: Welcome message

---
+++ src/com/bl/linked_list_data_structure/LinkedList.java
@@ -0,0 +1,9 @@
+package com.bl.linked_list_data_structure;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+
+        System.out.println("Linked List data structure implementation");
+    }
+}
